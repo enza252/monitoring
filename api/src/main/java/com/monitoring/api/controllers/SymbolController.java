67da78ab3be3ab519c5b7bcaf4adf2786805d738
ipe <ian.ensor-cic-uk@ibm.com>
2021-11-26 16:47:42 +0000

    add some logging, create logback, some cleanup and some validation

--- api/src/main/java/com/monitoring/api/controllers/SymbolController.java
+++ api/src/main/java/com/monitoring/api/controllers/SymbolController.java
@@ -2,17 +2,22 @@ package com.monitoring.api.controllers;
 
 import com.monitoring.api.entities.StockDataResponse;
 import com.monitoring.api.services.interfaces.SymbolServiceIF;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import static java.lang.String.format;
+import static java.util.Objects.isNull;
 
 @Controller
 public class SymbolController {
 
     private final SymbolServiceIF challengeService;
+    private static final Logger LOGGER = LoggerFactory.getLogger(SymbolController.class);
 
     @Autowired
     public SymbolController(SymbolServiceIF challengeService) {
@@ -20,9 +25,13 @@ public class SymbolController {
     }
 
     @RequestMapping(value = "/api/stockdata", method = RequestMethod.GET)
-    public @ResponseBody
-    StockDataResponse getStockData(@RequestParam("symbol") String symbol, @RequestParam("nDays") String nDays) {
-//        ToDoo add logger statement
-        return challengeService.calculateDataAndAverage(symbol, nDays);
+    public @ResponseBody ResponseEntity<StockDataResponse> getStockData(@RequestParam("symbol") String symbol, @RequestParam("nDays") String nDays) {
+        LOGGER.debug(format("Received request to fetch stock data for Symbol %s for the last nDays %s", symbol, nDays));
+        StockDataResponse stockDataResponse = challengeService.calculateDataAndAverage(symbol, nDays);
+        if (isNull(stockDataResponse)) {
+            return ResponseEntity.ok(null);
+        }
+        LOGGER.debug(format("Successfully fetched stock data for symbol %s for the last nDays %s", symbol, nDays));
+        return ResponseEntity.ok(stockDataResponse);
     }
 }
--- api/src/main/java/com/monitoring/api/services/SymbolService.java
+++ api/src/main/java/com/monitoring/api/services/SymbolService.java
@@ -7,6 +7,8 @@ import com.crazzyghost.alphavantage.timeseries.response.TimeSeriesResponse;
 import com.monitoring.api.config.ApiConfig;
 import com.monitoring.api.entities.StockDataResponse;
 import com.monitoring.api.services.interfaces.SymbolServiceIF;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.crazzyghost.alphavantage.Config;
@@ -16,11 +18,15 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static java.lang.Long.parseLong;
+import static java.lang.String.format;
+import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
 
 @Service
 public class SymbolService implements SymbolServiceIF {
 
     private final ApiConfig apiConfig;
+    private static final Logger LOGGER = LoggerFactory.getLogger(SymbolService.class);
 
     @Autowired
     public SymbolService(ApiConfig apiConfig) {
@@ -31,7 +37,6 @@ public class SymbolService implements SymbolServiceIF {
 
     @Override
     public StockDataResponse calculateDataAndAverage(String symbol, String nDays) {
-        long nDaysLong = parseLong(nDays);
         TimeSeriesResponse response = AlphaVantage.api()
                 .timeSeries()
                 .daily()
@@ -39,8 +44,16 @@ public class SymbolService implements SymbolServiceIF {
                 .outputSize(OutputSize.FULL)
                 .fetchSync();
 
+        if (nonNull(response.getErrorMessage())) {
+            LOGGER.error(format("Failed to obtain stock data for Symbol %s and nDays %s with error %s",
+                    symbol,
+                    nDays,
+                    response.getErrorMessage()));
+            return null;
+        }
+
         List<Double> lastNDays = response.getStockUnits().stream()
-                .limit(nDaysLong)
+                .limit(parseLong(nDays))
                 .map(StockUnit::getClose)
                 .collect(Collectors.toList());
 
