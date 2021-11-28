29c7c7fca24fa6bd14429636d46548df8e39f8a2
iane <mail@ianensor.info>
2021-11-28 00:33:42 +0000

    add userevent and test text input updates state

--- ui/__tests__/index.page.tsx
+++ ui/__tests__/index.page.tsx
@@ -1,5 +1,6 @@
 import Home from "../pages";
 import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
 
 describe("tests the index 'Home' page", () => {
 
@@ -9,10 +10,13 @@ describe("tests the index 'Home' page", () => {
     it("tests the correct title is displayed", () => {
         expect(screen.getByText("StockChecker")).toBeInTheDocument();
     })
-    it("tests the description is displayed", () => {
-        expect(screen.getByText("Simply enter your Stock Symbol (Ticker), such as")).toBeInTheDocument()
-    })
     it("tests the text field is present on the screen", () => {
         expect(screen.getByTestId("symbol-input-field")).toBeInTheDocument()
+    })
+    it("tests the input field accepts text and renders it once entered", () => {
+        const textFieldElement = screen.getByRole("textbox", { name: "symbol-input-field" })
+        expect(textFieldElement).toHaveValue("")
+        userEvent.type(textFieldElement, "BP")
+        expect(textFieldElement).toHaveValue("BP");
     })
 })
