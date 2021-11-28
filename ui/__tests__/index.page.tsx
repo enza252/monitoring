246048f81b7f3ce64126590955f97f367001d87c
iane <mail@ianensor.info>
2021-11-28 00:12:20 +0000

    add more checks

--- ui/__tests__/index.page.tsx
+++ ui/__tests__/index.page.tsx
@@ -2,8 +2,17 @@ import Home from "../pages";
 import { render, screen } from "@testing-library/react"
 
 describe("tests the index 'Home' page", () => {
-    it("tests the text field is present on the screen", () => {
+
+    beforeEach(() => {
         render(<Home/>)
+    })
+    it("tests the correct title is displayed", () => {
+        expect(screen.getByText("StockChecker")).toBeInTheDocument();
+    })
+    it("tests the description is displayed", () => {
+        expect(screen.getByText("Simply enter your Stock Symbol (Ticker), such as")).toBeInTheDocument()
+    })
+    it("tests the text field is present on the screen", () => {
         expect(screen.getByTestId("symbol-input-field")).toBeInTheDocument()
     })
 })
