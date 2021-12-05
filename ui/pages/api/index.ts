ce18f7e324304700b211b3d33f5d1c30b1fb4a3c
iane <mail@ianensor.info>
2021-12-05 15:09:05 +0000

    drop todo

--- ui/pages/api/index.ts
+++ ui/pages/api/index.ts
@@ -3,7 +3,6 @@ import axios from "axios"
 import { SymbolData } from "../../types"
 
 export const fetchStockData = (symbol :string, nDays :string) => {
-// TODO implement nginx and update this url
   return axios.get<SymbolData>(`/api/stockdata?symbol=${symbol}&nDays=${nDays}`)
   .then(response => response.data)
   .catch(error => console.error(error))
